9af15198de755b093f2c3a472311922098785e8e
Yuvraj <y,jaswal04@gmail.com>
2024-06-07 10:46:43 +0530

    connected to atlas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-require('dotenv').config({ path: './env'});
+require('dotenv').config({path:'./env'});
 
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const DB_NAME = require("./constants.js");
-const connectDB = require("../src/db/index.js");
+const { DB_NAME } = require("./constants.js");
+const connectDB = require("./db/index.js");
+const app = require("./app.js");
 
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 connectDB();
